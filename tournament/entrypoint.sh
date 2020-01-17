c871c44cc38e8c03a66f9c099cbd3f6047798d28
Harry Jubb <harry.jubb@gmail.com>
2020-01-17 23:18:17 +0000

    Auto superuser in app entrypoint.

--- tournament/entrypoint.sh
+++ tournament/entrypoint.sh
@@ -3,4 +3,9 @@
 set -e
 
 python manage.py migrate
+
+# Set up a superuser if one doesn't already exist
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('test', '', 'test') if not User.objects.filter(username='test').count() else 1" | python manage.py shell
+
+
 python manage.py runserver 0.0.0.0:8000
