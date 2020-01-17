47d171c50892962ed44ecc02295898e3d7dc7212
Harry Jubb <harry.jubb@gmail.com>
2020-01-17 23:18:36 +0000

    Basic event model.

--- tournament/tournament/apps/event/admin.py
+++ tournament/tournament/apps/event/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from tournament.apps.event.models import Event
 
-# Register your models here.
+
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    pass
--- tournament/tournament/settings.py
+++ tournament/tournament/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "tournament.apps.event",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
