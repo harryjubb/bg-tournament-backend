415b7e78c6ddd191dbfcf6b2c116be4ff023b94a
Harry Jubb <harry.jubb@gmail.com>
2020-01-18 18:28:26 +0000

    Working game data from BGG.

--- tournament/tournament/apps/game/models.py
+++ tournament/tournament/apps/game/models.py
@@ -2,11 +2,32 @@ from django.db import models
 
 from datetime import timedelta
 
+from tournament.apps.game.utils import get_bgg_data
+
+DEFAULTS = {
+    "name": "Name",
+    "game_type": "boardgame",
+    "min_age": 0,
+    "min_length": timedelta(minutes=5),
+    "max_length": timedelta(minutes=10),
+    "complexity": 2.0,
+    "min_players": 2,
+    "max_players": 5,
+    "url": "https://google.com",
+    "image_url": "https://via.placeholder.com/128",
+}
+
 
 class Game(models.Model):
     name = models.CharField(max_length=1024, blank=True)
-    min_length = models.DurationField(default=timedelta, blank=True)
-    max_length = models.DurationField(default=timedelta, blank=True)
+    game_type = models.CharField(
+        max_length=100,
+        choices=(("boardgame", "Board Game"), ("videogame", "Video Game")),
+        blank=True,
+    )
+    min_age = models.SmallIntegerField(blank=True)
+    min_length = models.DurationField(blank=True)
+    max_length = models.DurationField(blank=True)
     complexity = models.DecimalField(max_digits=3, decimal_places=2, blank=True)
     min_players = models.SmallIntegerField(blank=True)
     max_players = models.SmallIntegerField(blank=True)
@@ -20,7 +41,29 @@ class Game(models.Model):
     def save(self, *args, **kwargs):
 
         # Get data from BGG API if available
+        bgg_item = get_bgg_data(self.bgg_id)
 
-        # Otherwise set defaults: can't be null
+        for field in (
+            "name",
+            "game_type",
+            "min_age",
+            "min_length",
+            "max_length",
+            "complexity",
+            "min_players",
+            "max_players",
+            "url",
+            "image_url",
+        ):
+            if (
+                getattr(self, field) is None or getattr(self, field) == ""
+            ) and bgg_item[field] is not None:
+                setattr(self, field, bgg_item[field])
+            elif (
+                getattr(self, field) is None or getattr(self, field) == ""
+            ) and bgg_item[field] is None:
+                setattr(self, field, DEFAULTS[field])
+            else:
+                pass
 
         super().save(*args, **kwargs)
--- tournament/tournament/apps/game/utils.py
+++ tournament/tournament/apps/game/utils.py
@@ -1,6 +1,18 @@
 import requests
 import xmltodict
 
+import datetime
+
+
+def _cast_if_not_none(num, cast_func=int):
+    if num:
+        return cast_func(num)
+    return None
+
+
+def _mins_to_timedelta(mins):
+    return datetime.timedelta(minutes=int(mins))
+
 
 def get_bgg_data(bgg_id):
 
@@ -13,39 +25,47 @@ def get_bgg_data(bgg_id):
 
     result_dict = xmltodict.parse(result.content)
 
-    bgg_data = {
-        "id": bgg_id,
-        "url": f"https://boardgamegeek.com/boardgame/{id}",
-        "difficulty": result_dict.get("statistics", {})
-        .get("ratings", {})
-        .get("averageweight", {})
-        .get("_attributes", {})
-        .get("value", None),
-        "gameType": result_dict.get("_attributes", {}).get("type", None),
-        "imageUrl": result_dict.get("image", {}).get("_text", None),
-        "minimumAge": result_dict.get("minage", {})
-        .get("_attributes", {})
-        .get("value", None),
-        "name": getName(result_dict),  # TODO: write py version
-        "players": {
-            "minimum": result_dict.get("minplayers", {})
-            .get("_attributes", {})
-            .get("value", None),
-            "maximum": result_dict.get("maxplayers", {})
-            .get("_attributes", {})
-            .get("value", None),
-        },
-        "playtime": {
-            "minimum": result_dict.get("minplaytime", {})
-            .get("_attributes", {})
-            .get("value", None),
-            "maximum": result_dict.get("maxplaytime", {})
-            .get("_attributes", {})
-            .get("value", None),
-        },
-        "rating": result_dict.get("statistics", {})
-        .get("ratings", {})
-        .get("average", {})
-        .get("_attributes", {})
-        .get("value", None),
+    item = result_dict.get("items", {}).get("item", {})
+
+    item_data = {
+        "bgg_id": bgg_id,
+        "url": f"https://boardgamegeek.com/boardgame/{bgg_id}",
+        "complexity": _cast_if_not_none(
+            item.get("statistics", {})
+            .get("ratings", {})
+            .get("averageweight", {})
+            .get("@value", None),
+            float,
+        ),
+        "game_type": item.get("@type", None),
+        "image_url": item.get("image", None),
+        "min_age": _cast_if_not_none(item.get("minage", {}).get("@value", None), int),
+        "name": [
+            name["@value"]
+            for name in item.get("name", [])
+            if name["@type"] == "primary"
+        ][0]
+        if type(item["name"]) == list
+        else item.get("name", {}).get("@value", None),
+        "min_players": _cast_if_not_none(
+            item.get("minplayers", {}).get("@value", None), int
+        ),
+        "max_players": _cast_if_not_none(
+            item.get("maxplayers", {}).get("@value", None), int
+        ),
+        "min_length": _cast_if_not_none(
+            item.get("minplaytime", {}).get("@value", None), _mins_to_timedelta
+        ),
+        "max_length": _cast_if_not_none(
+            item.get("maxplaytime", {}).get("@value", None), _mins_to_timedelta
+        ),
+        "rating": _cast_if_not_none(
+            item.get("statistics", {})
+            .get("ratings", {})
+            .get("average", {})
+            .get("@value", None),
+            float,
+        ),
     }
+
+    return item_data
