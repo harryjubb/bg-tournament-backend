9398388a4d0dc8cbff1254045fe17b2f652525a7
Harry Jubb <harry.jubb@gmail.com>
2020-01-19 01:24:01 +0000

    Make score into a property method, avoid DB race conditions.

--- tournament/tournament/apps/play/models.py
+++ tournament/tournament/apps/play/models.py
@@ -8,18 +8,15 @@ class Play(models.Model):
     game = models.ForeignKey("game.Game", on_delete=models.CASCADE)
     winners = models.ManyToManyField("player.Player", related_name="won_plays")
     losers = models.ManyToManyField("player.Player", related_name="lost_plays")
-    score = models.FloatField()
 
-    def __str__(self):
-        return f"Play of {self.game.name} at {self.event.code} won by {', '.join(winner.name for winner in self.winners.all())}"
-
-    def save(self, *args, **kwargs):
-
-        self.score = score_play(
+    @property
+    def score(self):
+        return score_play(
             game_min_length=self.game.min_length,
             game_complexity=self.game.complexity,
             num_winners=self.winners.count(),
             num_losers=self.losers.count(),
         )
 
-        super().save(*args, **kwargs)
+    def __str__(self):
+        return f"{self.winners.count() + self.losers.count()} player {self.game.name} at {self.event.code} won by {', '.join(winner.name for winner in self.winners.all())}, scoring {self.score:.2f} per winner"
