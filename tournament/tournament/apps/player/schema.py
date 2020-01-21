c02425dd94c63a2b42354cf9fb74e8ce0004c373
Harry Jubb <harry.jubb@gmail.com>
2020-01-21 00:10:25 +0000

    Event aggregate resolvers.

--- tournament/tournament/apps/player/schema.py
+++ tournament/tournament/apps/player/schema.py
@@ -11,6 +11,21 @@ class PlayerType(DjangoObjectType):
         model = Player
         fields = ("id", "name")
 
+    event_play_count = graphene.Int(
+        event_code=graphene.String(),
+        description="Retrieve the number of plays for a given event code for this player.",
+    )
+
+    event_win_count = graphene.Int(
+        event_code=graphene.String(),
+        description="Retrieve the number of wins for a given event code for this player.",
+    )
+
+    event_loss_count = graphene.Int(
+        event_code=graphene.String(),
+        description="Retrieve the number of losses for a given event code for this player.",
+    )
+
     event_plays = graphene.List(
         "tournament.apps.play.schema.PlayType",
         event_code=graphene.String(),
@@ -28,6 +43,35 @@ class PlayerType(DjangoObjectType):
         event_code=graphene.String(),
         description="Retrieve plays for a given event code that this player lost.",
     )
+
+    event_total_score = graphene.Float(
+        event_code=graphene.String(),
+        description="Retrieve the total score accrued for a given event code for this player.",
+    )
+
+    def resolve_event_play_count(self, info, event_code=None):
+        return (
+            self.event_set.get(code=event_code)
+            .play_set.filter(Q(Q(winners__id=self.id) | Q(losers__id=self.id)))
+            .distinct()
+            .count()
+        )
+
+    def resolve_event_win_count(self, info, event_code=None):
+        return (
+            self.event_set.get(code=event_code)
+            .play_set.filter(winners__id=self.id)
+            .distinct()
+            .count()
+        )
+
+    def resolve_event_loss_count(self, info, event_code=None):
+        return (
+            self.event_set.get(code=event_code)
+            .play_set.filter(losers__id=self.id)
+            .distinct()
+            .count()
+        )
 
     def resolve_event_plays(self, info, event_code=None):
         return (
@@ -47,5 +91,13 @@ class PlayerType(DjangoObjectType):
         return (
             self.event_set.get(code=event_code)
             .play_set.filter(losers__id=self.id)
+            .distinct()
+        )
+
+    def resolve_event_total_score(self, info, event_code=None):
+        return sum(
+            play.score
+            for play in self.event_set.get(code=event_code)
+            .play_set.filter(winners__id=self.id)
             .distinct()
         )
