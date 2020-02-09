3c1e5ff696d715c9654a19f25563ce320ebc5c02
Harry Jubb <harry.jubb@gmail.com>
2020-02-09 17:20:22 +0000

    Play list display and filters.

--- tournament/tournament/apps/play/admin.py
+++ tournament/tournament/apps/play/admin.py
@@ -5,4 +5,6 @@ from tournament.apps.play.models import Play
 @admin.register(Play)
 class PlayAdmin(admin.ModelAdmin):
     filter_horizontal = ("winners", "losers")
+    list_display = ("__str__", "event", "date_created", "date_updated", "score")
+    list_filter = ("event", "date_created", "date_updated")
     readonly_fields = ("score", "date_created", "date_updated")
